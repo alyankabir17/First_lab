1391920f18f685763609b3a853cab41ea28658bb
Allyan Kabir <numl-s23-15654@numls.edu.pk>
2026-02-21 14:09:24 +0500

    Fix print statement syntax in jenkins.py

--- jenkins.py
+++ jenkins.py
@@ -1,3 +1,3 @@
 print("hello world")
 print(" alyan  is learning jenkins " )
-print(" check  this is showing to jenkins or not automatically and also checks trigger within minutes "
+print(" check  this is showing to jenkins or not automatically and also checks trigger within minutes ")
